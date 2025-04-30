eeed98014aff071e73b0a4f769c9014bfc72f760
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-04-30 08:30:21 +0530

    refactor: clean up settings and urls for JWT integration

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
-
-    
+    'rest_framework_simplejwt.token_blacklist',   
 ]
 
 MIDDLEWARE = [
