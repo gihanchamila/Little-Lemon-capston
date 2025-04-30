4e360c70e8ee69a281510c7de97e340defe24f00
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-04-30 08:26:43 +0530

    add JWT authentication endpoints for token management

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
@@ -23,4 +24,6 @@ urlpatterns = [
     path('api/', include('LittleLemonAPI.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
